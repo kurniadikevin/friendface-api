9ff62f7dac3933511b86f30cc255c22557a5bf7d
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-11-17 21:00:16 +0700

    update cors origin

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var commentsRouter = require('./routes/comments');
 
 var app = express();
 app.use(cors({
-  origin: "http://localhost:3000", // <-- location of the react app were connecting to
+  origin: "https://friendface-pd7stqqc8-kurniadikevin.vercel.app", // <-- location of the react app were connecting to
   credentials: true,
 }));
 
