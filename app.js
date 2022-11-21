bb02d39b223716d7d44aab38225c1830878f09eb
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-11-21 19:38:56 +0700

    allow cors

--- app.js
+++ app.js
@@ -33,10 +33,7 @@ var commentsRouter = require('./routes/comments');
 
 
 var app = express();
-app.use(cors({
-  origin: ["https://friendface-pd7stqqc8-kurniadikevin.vercel.app","http://localhost:3000","https://friendface.vercel.app/"], // <-- location of the react app were connecting to
-  credentials: true,
-}));
+app.use(cors());
 
 //integrate MONGO DB
 const mongoose = require("mongoose");
