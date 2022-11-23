66c4174c05b8a715518b9232ede4d2277242c797
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-11-23 11:17:17 +0700

    fix redirect and source db

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(cors({origin: 'https://friendface.vercel.app/',
 
 //integrate MONGO DB
 const mongoose = require("mongoose");
-const dev_db_url = 'mongodb+srv://kurniadikevin:pisausl@cluster0.vqkgg6n.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB = process.env.MONGODB_URI || dev_db_url;  
+const mongoDB = 'mongodb+srv://kurniadikevin:pisausl@cluster0.vqkgg6n.mongodb.net/?retryWrites=true&w=majority'
+/* const mongoDB = process.env.MONGODB_URI || dev_db_url;   */
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
