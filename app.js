a4b0428fcd34d362ff56278867c8789890d07041
kurniadikevin <kurniadikevin@gmail.com>
2023-04-04 12:39:10 +0700

    hide db url to env

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cors = require('cors');
 const compression = require("compression");
 const helmet = require("helmet");
-
+require('dotenv').config();
 const bcrypt =require('bcryptjs');
 const session = require("express-session");
 const passport = require("passport");
@@ -44,8 +44,7 @@ app.use(cors({
 
 //integrate MONGO DB
 const mongoose = require("mongoose");
-const dev_db_url = 'mongodb+srv://kurniadikevin:pisausl@cluster0.vqkgg6n.mongodb.net/?retryWrites=true&w=majority'
- const mongoDB = process.env.MONGODB_URI || dev_db_url;   
+ const mongoDB = process.env.DATABASE_URL;   
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
